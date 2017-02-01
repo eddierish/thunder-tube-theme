38541f5467b1f57d3e274cedb1650fb2de4ffe68
Philip Howley <howley.phil@gmail.com>
2017-02-01 15:56:35 -0800

    update db, pull data with blog_info, google fonts, menus

--- footer.php
+++ footer.php
@@ -2,6 +2,19 @@
 
 <footer>
   <p>&copy; Company 2017</p>
+  <?php
+  $defaults = array(
+    'theme_location'  => 'secondary-menu',
+    'container'       => 'nav',
+    'menu_class'      => 'footer-navigation',
+    'echo'            => true,
+    'fallback_cb'     => 'wp_page_menu',
+    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+    'depth'           => 0,
+    'walker'          => ''
+  );
+  wp_nav_menu( $defaults );
+  ?>
 </footer>
 </div> <!-- /container -->
 
